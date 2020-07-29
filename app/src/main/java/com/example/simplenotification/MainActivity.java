476f87ad36e3d23739974ef3fb02de43d093d699
adhiiisetiawan <adhisetiawan518@gmail.com>
2020-07-29 14:42:08 +0700

    add notification manager to MainActivity

--- app/src/main/java/com/example/simplenotification/MainActivity.java
+++ app/src/main/java/com/example/simplenotification/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.simplenotification;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int NOTIFICATION_ID = 1;
+    public static String CHANNEL_ID = "channel_01";
+    public static CharSequence CHANNEL_NAME = "my channel";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.baseline_notifications_white_48dp)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.baseline_notifications_white_48dp))
+                .setContentTitle(getResources().getString(R.string.content_title))
+                .setContentText(getResources().getString(R.string.content_text))
+                .setSubText(getResources().getString(R.string.subtext))
+                .setAutoCancel(true);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription(CHANNEL_NAME.toString());
+            builder.setChannelId(CHANNEL_ID);
+            if (notificationManager != null){
+                notificationManager.createNotificationChannel(channel);
+            }
+        }
+
+
+        Notification notification = builder.build();
+
+        if (notificationManager != null){
+            notificationManager.notify(NOTIFICATION_ID, notification);
+        }
     }
 }
